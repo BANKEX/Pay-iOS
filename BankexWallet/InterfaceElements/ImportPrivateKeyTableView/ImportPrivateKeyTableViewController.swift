849c59d609a1a54717d8a41b4c44814abb68d892
Alex Vlasov <shamatar@github.com>
2018-02-08 20:21:50 +0300

    Second

---
+++ BankexWallet/InterfaceElements/ImportPrivateKeyTableView/ImportPrivateKeyTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ImportPrivateKeyTableViewController.swift
+//  BankexWallet
+//
+//  Created by Alexander Vlasov on 07.02.2018.
+//  Copyright © 2018 Alexander Vlasov. All rights reserved.
+//
+
+import Foundation
