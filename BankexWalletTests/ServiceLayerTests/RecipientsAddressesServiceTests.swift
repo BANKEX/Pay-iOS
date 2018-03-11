8e3d2ac01380c2ee8b09e000b0330c2f513e72e6
Ekaterina Korovkina <c.korovkina@gmail.com>
2018-03-11 19:37:56 +0700

    Address book imitation

---
+++ BankexWalletTests/ServiceLayerTests/RecipientsAddressesServiceTests.swift
@@ -0,0 +1,61 @@
+//
+//  RecipientsAddressesServiceTests.swift
+//  BankexWalletTests
+//
+//  Created by Korovkina, Ekaterina on 3/11/2561 BE.
+//  Copyright © 2561 Alexander Vlasov. All rights reserved.
+//
+
+import XCTest
+@testable import BankexWallet
+
+class RecipientsAddressesServiceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testStoreAndRetrieveData() {
+        //given
+        let service = RecipientsAddressesServiceImplementation()
+        service.store(address: "Some Another String", with: "Second Name")
+        service.store(address: "Some String", with: "First Name")
+        service.store(address: "Some afgerg String", with: "Third Name")
+
+        //when
+        let allAddresses = service.getAllStoredAddresses()
+        
+        //then
+        XCTAssertEqual(allAddresses?.count, 3)
+        XCTAssertEqual(allAddresses?.first?.0, "First Name")
+        XCTAssertEqual(allAddresses?.last?.0, "Third Name")
+
+        service.clearAllSavedAddresses()
+    }
+    
+    func testDeletAddress() {
+        //given
+        let service = RecipientsAddressesServiceImplementation()
+        service.store(address: "Some Another String", with: "Second Name")
+        service.store(address: "Some String", with: "First Name")
+        service.store(address: "Some afgerg String", with: "Third Name")
+        
+        //when
+        service.delete(with: "First Name")
+        let allAddresses = service.getAllStoredAddresses()
+        
+        //then
+        XCTAssertEqual(allAddresses?.count, 2)
+        XCTAssertEqual(allAddresses?.first?.0, "Second Name")
+        XCTAssertEqual(allAddresses?.last?.0, "Third Name")
+        
+        service.clearAllSavedAddresses()
+    }
+    
+}
