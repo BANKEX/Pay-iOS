ab9b26eedb431596715e01441f9a31abe64cff80
Ekaterina Korovkina <c.korovkina@gmail.com>
2018-03-11 18:26:45 +0700

    Added service for single key wallets

---
+++ BankexWallet/ServiceLayer/KeysService.swift
@@ -0,0 +1,98 @@
+//
+//  KeysService.swift
+//  BankexWallet
+//
+//  Created by Korovkina, Ekaterina on 3/11/2561 BE.
+//  Copyright © 2561 Alexander Vlasov. All rights reserved.
+//
+
+import UIKit
+import web3swift
+
+//TODO: Part of these calls should throw
+protocol SingleKeyService {
+    
+    func createNewSingleAddressWallet(withPassword password: String?)
+    func createNewSingleAddressWallet(fromText privateKey: String)
+    func fullListOfPublicAddresses() -> [String]?
+    func preferredSingleAddress() -> String?
+    func updatePreferred(address: String)
+    
+}
+
+class SingleKeyServiceImplementation: SingleKeyService {
+    
+    let userDefaultsKeyForSelectedAddress = "SelectedAddress"
+    
+    let pathForKeys: String
+    let defaultPassword: String
+    init(pathToStoreKeys: String = "/keystore", defaultPassword: String = "BANKEXFOUNDATION") {
+        self.pathForKeys = pathToStoreKeys
+        self.defaultPassword = defaultPassword
+    }
+    
+    func preferredSingleAddress() -> String? {
+        guard let selectedAddress = UserDefaults.standard.string(forKey: userDefaultsKeyForSelectedAddress) else {
+            if let address = fullListOfPublicAddresses()?.first {
+                return address
+            }
+            return nil
+        }
+        return selectedAddress
+    }
+    
+    func updatePreferred(address: String) {
+        UserDefaults.standard.set(address, forKey: userDefaultsKeyForSelectedAddress)
+    }
+    
+    func fullListOfSingleEthereumAddresses() -> [EthereumAddress]? {
+        let userDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let keystoreManager = KeystoreManager.managerForPath(userDir + pathForKeys)
+        
+        return keystoreManager?.addresses
+    }
+    
+    func fullListOfPublicAddresses() -> [String]? {
+        let userDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let keystoreManager = KeystoreManager.managerForPath(userDir + pathForKeys)
+        
+        return keystoreManager?.addresses?.flatMap{ (ethAddress) -> String in
+            return ethAddress.address
+        }
+    }
+    
+    func createNewSingleAddressWallet(withPassword password: String? = nil) {
+        let usingPassword = password ?? defaultPassword
+        guard let newWallet = try? EthereumKeystoreV3(password: usingPassword) else {return}
+        guard let wallet = newWallet, wallet.addresses?.count == 1 else {return}
+        guard let keydata = try? JSONEncoder().encode(wallet.keystoreParams) else {return}
+        guard let address = newWallet?.addresses?.first?.address else {return}
+        save(keyData: keydata, for: address)
+    }
+    
+    func createNewSingleAddressWallet(fromText privateKey: String) {
+        let text = privateKey.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        guard let data = Data.fromHex(text) else {return}
+        guard let newWallet = try? EthereumKeystoreV3(privateKey:data) else {return}
+        guard let wallet = newWallet, wallet.addresses?.count == 1 else {return}
+        guard let keydata = try? JSONEncoder().encode(wallet.keystoreParams) else {return}
+        guard let address = newWallet?.addresses?.first?.address else {return}
+        save(keyData: keydata, for: address)
+    }
+    
+    private func save(keyData: Data, for address: String) {
+        let userDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let path = userDir + pathForKeys
+        let fileManager = FileManager.default
+        var isDir : ObjCBool = false
+        var exists = fileManager.fileExists(atPath: path, isDirectory: &isDir)
+        if (!exists && !isDir.boolValue){
+            try? fileManager.createDirectory(atPath: path, withIntermediateDirectories: true, attributes: nil)
+            exists = fileManager.fileExists(atPath: path, isDirectory: &isDir)
+        }
+        if (!isDir.boolValue) {
+            return
+        }
+        FileManager.default.createFile(atPath: path + "/" + address + ".json", contents: keyData, attributes: nil)
+    }
+}
