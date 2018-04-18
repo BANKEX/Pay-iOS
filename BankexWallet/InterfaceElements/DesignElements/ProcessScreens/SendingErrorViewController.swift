d72a54d5db1d469c97ba6cfe9640c995b9a8141d
Ekaterina Korovkina <c.korovkina@gmail.com>
2018-04-19 03:00:48 +0700

    Added logic to error screen

--- BankexWallet/InterfaceElements/DesignElements/ProcessScreens/SendingErrorViewController.swift
+++ BankexWallet/InterfaceElements/DesignElements/ProcessScreens/SendingErrorViewController.swift
@@ -8,28 +8,21 @@
 
 import UIKit
 
+protocol Retriable {
+    func retryExisitngTransaction()
+}
+
 class SendingErrorViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func retryTransaction(_ sender: Any) {
+        let count = navigationController?.viewControllers.count ?? 2
+        if let previousController = navigationController?.viewControllers[count - 2] as? Retriable {
+            previousController.retryExisitngTransaction()
+        }
+        navigationController?.popViewController(animated: false)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: false)
     }
-    */
-
 }
