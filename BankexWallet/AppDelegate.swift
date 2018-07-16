8e3517f67ef514def9c9ffbed7b4878eee7cc1dc
Vladislav <vladaccess@outlook.com>
2018-07-16 18:24:01 +0300

    fix config

--- BankexWallet/AppDelegate.swift
+++ BankexWallet/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        Crashlytics.start(withAPIKey: "5b2cfd1743e96d92261c59fb94482a93c8ec4e13")
         Fabric.with([Crashlytics.self])
         let initialRouter = InitialLogicRouter()
         guard let navigationController = window?.rootViewController as? UINavigationController else {
