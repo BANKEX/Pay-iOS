d210a1f3503d78d320e79d4bfc5ab34620b3344d
Fesenko Georgy <fesenko.g@gmail.com>
2018-07-16 17:49:36 +0300

    files added

---
+++ BankexWallet/InterfaceElements/DesignElements/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  OnboardingViewController.swift
+//  BankexWallet
+//
+//  Created by Георгий Фесенко on 16/07/2018.
+//  Copyright © 2018 Alexander Vlasov. All rights reserved.
+//
+
+import Foundation
