7fcf231e8725ebfabf70e99928c7499c078deb61
Vladislav <vladaccess@outlook.com>
2018-07-23 13:56:22 +0300

    fix bugs

--- BankexWallet/InterfaceElements/DesignElements/WalletSettings/Networks/CreateNetworkViewController.swift
+++ BankexWallet/InterfaceElements/DesignElements/WalletSettings/Networks/CreateNetworkViewController.swift
@@ -8,26 +8,71 @@
 
 import UIKit
 
-class CreateNetworkViewController: UITableViewController {
+class CreateNetworkViewController: UITableViewController,UITextFieldDelegate {
     
+    
+    enum State {
+        case available,unavailable
+    }
+    
+    
+    
+    //MARK: - IBOutlets
     @IBOutlet weak var networkURLTextField:UITextField!
     @IBOutlet weak var networkNameTextField:UITextField!
     
-
+    
+    var joinButton = UIBarButtonItem(title: "Join", style: .done, target: self, action: #selector(joinToConnection))
+    var state:State = .unavailable {
+        didSet {
+            if state == .available {
+                joinButton.isEnabled = true
+                [networkNameTextField,networkURLTextField].forEach { tf in
+                    tf?.returnKeyType = .done
+                }
+            }else {
+                joinButton.isEnabled = false
+                [networkNameTextField,networkURLTextField].forEach { tf in
+                    tf?.returnKeyType = .next
+                }
+            }
+        }
+    }
+    
+    //MARK: - LifeCircle
     override func viewDidLoad() {
         super.viewDidLoad()
+        state = .unavailable
+        [networkNameTextField,networkURLTextField].forEach { (tf) in
+            tf?.delegate = self
+        }
         navigationItem.title = "New Network"
         tableView.separatorStyle = .none
         networkURLTextField.becomeFirstResponder()
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(back))
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Join", style: .done, target: self, action: #selector(joinToConnection))
+        navigationItem.rightBarButtonItem = joinButton
+        tableView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(resignTextFields)))
     }
     
+    
+    
+    
+    
+    
+    
+    //MARK: - Methods
     @objc func textFromBuffer() {
         if let text = UIPasteboard.general.string {
             networkURLTextField.text = text
         }
     }
+    
+    @objc func resignTextFields() {
+        for tf in [networkURLTextField,networkNameTextField] {
+            tf?.resignFirstResponder()
+        }
+    }
+    
     
     @objc func back() {
         navigationController?.popViewController(animated: true)
@@ -37,7 +82,7 @@ class CreateNetworkViewController: UITableViewController {
         //TODO
     }
 
-    
+    //MARK: - DataSource
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 54.0
     }
@@ -55,4 +100,40 @@ class CreateNetworkViewController: UITableViewController {
             return nil
         }
     }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.returnKeyType == .done && joinButton.isEnabled {
+            //Join
+        }else if textField.returnKeyType == .next {
+            if textField == networkURLTextField {
+                networkNameTextField.becomeFirstResponder()
+            }else {
+                networkURLTextField.becomeFirstResponder()
+            }
+        }
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if joinButton.isEnabled {
+            textField.returnKeyType = .done
+        }else {
+            textField.returnKeyType = .next
+        }
+    }
+    
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentText = (textField.text ?? "")  as NSString
+        let futureString = currentText.replacingCharacters(in: range, with: string) as String
+        if networkURLTextField.text != "" && networkNameTextField.text != "" {
+            state = .available
+        }else {
+            state = .unavailable
+        }
+        return true
+    }
+    
+    
 }
