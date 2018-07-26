92b7a78817298e6445cecfaad7dfd7b52df9bf3f
Vladislav <vladaccess@outlook.com>
2018-07-26 16:22:52 +0300

    add logic to textFields

--- BankexWallet/InterfaceElements/DesignElements/Contacts/AddContactViewController.swift
+++ BankexWallet/InterfaceElements/DesignElements/Contacts/AddContactViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import AVFoundation
+import web3swift
 
-class AddContactViewController: UITableViewController {
+class AddContactViewController: UITableViewController,UITextFieldDelegate {
     
     @IBOutlet weak var firstNameTextField:UITextField?
     @IBOutlet weak var lastNameTextField:UITextField?
@@ -32,17 +34,27 @@ class AddContactViewController: UITableViewController {
     }
     
     var headerView:UIView!
+    let heightOfRow:CGFloat = 47.0
     
-    var doneButton:UIBarButtonItem {
-        return UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(done))
-    }
-
+    var doneButton:UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(done))
+    
+    var service = RecipientsAddressesServiceImplementation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        state = .noAvailable
+        firstNameTextField?.becomeFirstResponder()
+        tableView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(removeBecomeResponser)))
+        setupTextFields()
         setupNavBar()
         setupHeader()
         setupFooter()
+    }
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        state = .noAvailable
     }
     
     func setupNavBar() {
@@ -52,8 +64,23 @@ class AddContactViewController: UITableViewController {
         navigationItem.rightBarButtonItem = doneButton
     }
     
+    func setupTextFields() {
+        [addressTextField,firstNameTextField,lastNameTextField].forEach { tf in
+            tf?.delegate = self
+            tf?.autocorrectionType = .no
+        }
+    }
+    
     func setupFooter() {
-        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.width, height: tableView.bounds.height))
+        var heightOfFooter:CGFloat!
+        if #available(iOS 11.0, *) {
+            if let bottomInset = UIApplication.shared.keyWindow?.safeAreaInsets.bottom,let topInset = UIApplication.shared.keyWindow?.safeAreaInsets.top {
+                heightOfFooter = UIScreen.main.bounds.height - (heightOfRow * 3) - headerView.bounds.height - (navigationController?.navigationBar.bounds.height)! - topInset - bottomInset
+            }
+        }else {
+            heightOfFooter = UIScreen.main.bounds.height - (heightOfRow * 3) - headerView.bounds.height - (navigationController?.navigationBar.bounds.height)!
+        }
+        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.width, height: heightOfFooter))
         let pasteButton = UIButton(frame: CGRect(x: 16, y: 15, width: 140.0, height: 28.0))
         pasteButton.setImage(#imageLiteral(resourceName: "Paste button"), for: .normal)
         pasteButton.addTarget(self, action: #selector(importFromBuffer(_:)), for: .touchUpInside)
@@ -73,7 +100,19 @@ class AddContactViewController: UITableViewController {
     }
     
     @objc func done() {
-        //TODO
+        //Check address
+//        guard let ethAddress = EthereumAddress(addressTextField?.text ?? "") else {
+//            //Show error
+//        }
+//        service.store(address: addressTextField?.text!, with: firstNameTextField?.text!, isEditing: false) { (error) in
+//
+//        }
+    }
+    
+    @objc func removeBecomeResponser() {
+        [addressTextField,firstNameTextField,lastNameTextField].forEach { tf in
+            tf?.resignFirstResponder()
+        }
     }
     
     fileprivate func createCircle() {
@@ -99,73 +138,63 @@ class AddContactViewController: UITableViewController {
     
     
     @objc func importFromBuffer(_ sender:Any) {
-        //TODO
+        if let text = UIPasteboard.general.string {
+            addressTextField?.text = text
+            if !(firstNameTextField?.text?.isEmpty ?? true) && !(lastNameTextField?.text?.isEmpty ?? true) {
+                state = .available
+            }else {
+                state = .noAvailable
+            }
+        }
+        
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 47.0
+        return heightOfRow
     }
     
     override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return 0.0001
     }
     
-    
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentString = (textField.text ?? "") as NSString
+        let futureString = currentString.replacingCharacters(in: range, with: string) as String
+        switch textField {
+        case firstNameTextField:
+            if !futureString.isEmpty && !(lastNameTextField?.text?.isEmpty ?? true) && !(addressTextField?.text?.isEmpty ?? true) {
+                state = .available
+            }else {
+                state = .noAvailable
+            }
+        case lastNameTextField:
+            if !futureString.isEmpty && !(firstNameTextField?.text?.isEmpty ?? true) && !(addressTextField?.text?.isEmpty ?? true) {
+                state = .available
+            }else {
+                state = .noAvailable
+            }
+        case addressTextField:
+            if !futureString.isEmpty && !(firstNameTextField?.text?.isEmpty ?? true) && !(lastNameTextField?.text?.isEmpty ?? true) {
+                state = .available
+            }else {
+                state = .noAvailable
+            }
+        default:
+            state = .noAvailable
+        }
         return true
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+
+
