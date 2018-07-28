78f380993b460c9b5e3d8e0d7d54b88530a84c1b
Vladislav <vladaccess@outlook.com>
2018-07-28 23:02:47 +0300

    configure field

--- BankexWallet/InterfaceElements/DesignElements/Contacts/ProfileContactViewController.swift
+++ BankexWallet/InterfaceElements/DesignElements/Contacts/ProfileContactViewController.swift
@@ -8,32 +8,64 @@
 
 import UIKit
 
-class ProfileContactViewController: UITableViewController {
+class ProfileContactViewController: UITableViewController,UITextFieldDelegate {
     
     @IBOutlet weak var addressTextField:UITextField?
     @IBOutlet weak var noteTextView:UITextView?
     
     
+    enum State {
+        case Editable,notEditable
+    }
+    
     
     
     var selectedContact:FavoriteModel!
-
+    var state:State = .notEditable
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTextField()
+        configureTableView()
+    }
+    
+    func configureTableView() {
+        tableView.allowsSelection = false
         tableView.tableFooterView = UIView(frame: .zero)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateUI()
+    }
+    
+    func configureTextField() {
+        addressTextField?.borderStyle = .none
+        addressTextField?.delegate = self
+        addressTextField?.textColor = WalletColors.blueText.color()
+        addressTextField?.autocorrectionType = .no
+        addressTextField?.autocapitalizationType = .none
+    }
+    
+    func updateUI() {
+        addressTextField?.text = selectedContact.address
+    }
+    
     @IBAction func sendFunds() {
-        
+        //TODO
     }
     
     @IBAction func shareContact() {
-        
+        //TODO
     }
     
     @IBAction func removeContact() {
-        
+        //TODO
     }
     
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return state == .Editable ? true : false
+    }
 
 }
