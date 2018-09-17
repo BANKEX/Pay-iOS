a207e69dc1d3e114c6c2c60fd27b05fb8cd912bd
Vladislav <vladaccess@outlook.com>
2018-09-17 13:55:06 +0300

    add animation

--- BankexWallet/InterfaceElements/DesignElements/Home/Controller/CreateTokenController.swift
+++ BankexWallet/InterfaceElements/DesignElements/Home/Controller/CreateTokenController.swift
@@ -17,6 +17,7 @@ class CreateTokenController: BaseViewController {
     @IBOutlet weak var bottomContraint:NSLayoutConstraint!
     @IBOutlet weak var pasteButton:UIButton!
     @IBOutlet weak var qrButton:UIButton!
+    @IBOutlet weak var addTokenLbl:UILabel!
     
     var needAddTokenAnimation = false
     
@@ -69,6 +70,7 @@ class CreateTokenController: BaseViewController {
         }
         DispatchQueue.main.async {
             if self.needAddTokenAnimation {
+                self.addTokenLbl.text = "Token was added to your wallet"
                 self.needAddTokenAnimation = false
                 UIView.animate(withDuration: 0.6,animations: {
                     if #available(iOS 11.0, *) {
@@ -198,6 +200,25 @@ extension CreateTokenController:UITableViewDataSource,UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let num = floor(Double(indexPath.row/2))
         let tokenToAdd = self.tokensList![Int(num)]
+        if tokenToAdd.isAdded {
+            addTokenLbl.text = "Token is already added to your wallet"
+            UIView.animate(withDuration: 0.6,animations: {
+                if #available(iOS 11.0, *) {
+                    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                    let bottomInset = appDelegate.window?.safeAreaInsets.bottom
+                    self.bottomContraint.constant = bottomInset!
+                }else {
+                    self.bottomContraint.constant = 0
+                }
+                self.view.layoutIfNeeded()
+            }) { (_) in
+                UIView.animate(withDuration: 0.7,delay:0.5,animations: {
+                    self.bottomContraint.constant = -58.0
+                    self.view.layoutIfNeeded()
+                })
+            }
+            return
+        }
         chosenToken = tokenToAdd
         performSegue(withIdentifier: "addChosenToken", sender: self)
     }
