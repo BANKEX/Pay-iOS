2a9352c82cd44b9f6910567a0901999f7f56504d
Andrew Kozlov <a.kozlov@bankex.com>
2018-11-28 11:58:05 +0300

    AssetManagementContacts added

---
+++ BankexWallet/AssetManagement/Contacts/AssetManagementContactsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AssetManagementContactsViewController.swift
+//  BankexWallet
+//
+//  Created by Andrew Kozlov on 28/11/2018.
+//  Copyright © 2018 Alexander Vlasov. All rights reserved.
+//
+
+import UIKit
+
+class AssetManagementContactsViewController: UIViewController {
+    
+}
