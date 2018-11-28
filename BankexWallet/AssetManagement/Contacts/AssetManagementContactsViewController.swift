dda6298cf4f1f52f848ee7ec3cfbc413d8f2b6bf
Andrew Kozlov <a.kozlov@bankex.com>
2018-11-28 12:34:18 +0300

    AssetManagementContacts implemented

--- BankexWallet/AssetManagement/Contacts/AssetManagementContactsViewController.swift
+++ BankexWallet/AssetManagement/Contacts/AssetManagementContactsViewController.swift
@@ -7,11 +7,42 @@
 //
 
 import UIKit
+import MessageUI
 
 class AssetManagementContactsViewController: UIViewController {
     
+    @IBAction func sendEmail() {
+        let mailComposeViewController = MFMailComposeViewController()
+        mailComposeViewController.mailComposeDelegate = self
+        mailComposeViewController.setToRecipients(["support@bankex.com"])
+        mailComposeViewController.setSubject("")
+        mailComposeViewController.setMessageBody("", isHTML: false)
+        
+        present(mailComposeViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func openTelegram() {
+        let channelURL = URL.init(string: "tg://resolve?domain=bankex")!
+        
+        UIApplication.shared.openURL(channelURL)
+    }
+    
+    @IBAction func openPage() {
+        let pageURL = URL(string: "https://bankex.com/en/sto/asset-management")!
+        
+        UIApplication.shared.openURL(pageURL)
+    }
+    
     @IBAction func finish() {
         performSegue(withIdentifier: "Home", sender: self)
+    }
+    
+}
+
+extension AssetManagementContactsViewController: MFMailComposeViewControllerDelegate {
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
     }
     
 }
