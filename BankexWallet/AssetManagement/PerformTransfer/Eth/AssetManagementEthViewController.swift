eb9fbc9fff0c51bf986b275f4dfa351fa9bb24d7
Oleg Kolomyitesv <o.kolomyitsev@bankex.com>
2018-11-28 16:14:02 +0300

    AssetManagementEthViewController was added

---
+++ BankexWallet/AssetManagement/PerformTransfer/Eth/AssetManagementEthViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AssetManagementPerformEthTransferViewController.swift
+//  BankexWallet
+//
+//  Created by Oleg Kolomyitsev on 28/11/2018.
+//  Copyright © 2018 Alexander Vlasov. All rights reserved.
+//
+
+import UIKit
+
+class AssetManagementEthViewController: UIViewController {
+        
+}
+
