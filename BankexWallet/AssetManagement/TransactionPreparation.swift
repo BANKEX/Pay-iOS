faa5eb405ce5588f205773cb3d13ecee4698987c
Vladislav <vladaccess@outlook.com>
2018-11-28 22:54:49 +0300

    added calculation of gasPrice

--- BankexWallet/AssetManagement/TransactionPreparation.swift
+++ BankexWallet/AssetManagement/TransactionPreparation.swift
@@ -21,7 +21,6 @@ class PreparetionTransaction {
     var amount:String
     var toAddress:String
     var transactionIntermediate:TransactionIntermediate?
-    let gasPrice:Double = 15
     let gasLimit = "21001"
     
     init(toAddress to:String, amount:String) {
@@ -35,27 +34,61 @@ class PreparetionTransaction {
             switch result {
             case .Success(let trans):
                 self.transactionIntermediate = trans
-                let options = self.getWeb3options()
-                guard let transIntermediate = self.transactionIntermediate else { return }
-                self.sendETHService.send(transactionModel: transactionETHModel, transaction: transIntermediate, options: options) { _ in }
+                self.getWeb3options(complited: { options in
+                    guard let transIntermediate = self.transactionIntermediate else { return }
+                    self.sendETHService.send(transactionModel: transactionETHModel, transaction: transIntermediate, options: options) { _ in }
+                })
             case .Error(let error):
                 printDebug(error.localizedDescription)
             }
         }
     }
     
+    func requestGasPrice(onComplition:@escaping (Double) -> Void) {
+        let path = "https://ethgasstation.info/json/ethgasAPI.json"
+        guard let url = URL(string: path) else {
+            DispatchQueue.main.async {
+                onComplition(0)
+            }
+            return
+        }
+        let dataTask = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                printDebug(error.localizedDescription)
+                DispatchQueue.main.async {
+                    onComplition(0)
+                }
+                return
+            }
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+                    let gasPrice = json["average"] as! Double
+                    DispatchQueue.main.async {
+                        onComplition(gasPrice)
+                    }
+                }catch {
+                    DispatchQueue.main.async {
+                        onComplition(0)
+                    }
+                }
+            }
+        }
+        dataTask.resume()
+    }
     
     
-    
-    
-    private func getWeb3options() -> Web3Options {
+    private func getWeb3options(complited:@escaping (Web3Options) -> ()) {
         var options = Web3Options.defaultOptions()
         options.from = transactionIntermediate?.options?.from
         options.to = transactionIntermediate?.options?.to
         options.value = transactionIntermediate?.options?.value
-        return options
+        options.gasLimit = gasLimit.toBigInt
+        requestGasPrice { gasPrice in
+            options.gasPrice = BigUInt(gasPrice.toWei)
+            complited(options)
+        }
     }
-    
     
     private func createETHTransactionModel() -> ETHTransactionModel? {
         let ethToken = createETHModel()
@@ -73,5 +106,11 @@ class PreparetionTransaction {
 extension String {
     var toBigInt:BigUInt? {
         return BigUInt(self)
+    }
+}
+
+extension Double {
+    var toWei:Double {
+        return self * pow(10, 9)
     }
 }
