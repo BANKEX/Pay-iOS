4dad6c3c1c7e0a40204fc8835db4e25759ec1927
Andrew Kozlov <a.kozlov@bankex.com>
2018-11-29 15:23:32 +0300

    AssetManagementEthSuccess added

---
+++ BankexWallet/AssetManagement/PerformTransfer/EthSuccess/AssetManagementEthSuccessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AssetManagementEthSuccessViewController.swift
+//  BankexWallet
+//
+//  Created by Andrew Kozlov on 29/11/2018.
+//  Copyright © 2018 Alexander Vlasov. All rights reserved.
+//
+
+import UIKit
+
+class AssetManagementEthSuccessViewController: UIViewController {
+    
+}
