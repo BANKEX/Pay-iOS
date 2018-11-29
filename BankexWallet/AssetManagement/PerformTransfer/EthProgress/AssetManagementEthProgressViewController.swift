6d0c5bd5db6c21cf3871b39493ef387140d19db8
Vladislav <vladaccess@outlook.com>
2018-11-29 16:22:08 +0300

    added sendTr flow

--- BankexWallet/AssetManagement/PerformTransfer/EthProgress/AssetManagementEthProgressViewController.swift
+++ BankexWallet/AssetManagement/PerformTransfer/EthProgress/AssetManagementEthProgressViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class AssetManagementEthProgressViewController: UIViewController {
     
+    var sendTrService:SendTransactionService!
+    var amount:String?
+    var toAddress:String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let amount = amount else { return }
+        sendTrService = SendTransactionService(toAddress: toAddress, amount: amount)
+        sendTrService.sendTransaction { result in
+            switch result {
+            case .Success(let transactionResult):
+                break
+            case .Error(let error):
+                break
+            }
+        }
+    }
+    
     private func showSuccess() {
         performSegue(withIdentifier: "Success", sender: self)
     }
