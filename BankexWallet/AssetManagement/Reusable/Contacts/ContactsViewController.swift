93586eb80aeb39a2190d2f8ef6e27f05efdf2d76
Oleg Kolomyitesv <o.kolomyitsev@bankex.com>
2018-11-29 18:25:05 +0300

    Reusable contacts wireframe viewController was added

---
+++ BankexWallet/AssetManagement/Reusable/Contacts/ContactsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ContactsViewController.swift
+//  BankexWallet
+//
+//  Created by Oleg Kolomyitsev on 29/11/2018.
+//  Copyright © 2018 Alexander Vlasov. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+class ContactViewController: UIViewController {
+    
+    var trHash: String?
+    
+    @IBAction func sendEmail() {
+        let mailComposeViewController = MFMailComposeViewController()
+        mailComposeViewController.mailComposeDelegate = self
+        mailComposeViewController.setToRecipients(["support@bankex.com"])
+        mailComposeViewController.setSubject("")
+        mailComposeViewController.setMessageBody("", isHTML: false)
+        
+        present(mailComposeViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func openTelegram() {
+        let channelURL = URL.init(string: "tg://resolve?domain=bankex")!
+        
+        UIApplication.shared.openURL(channelURL)
+    }
+    
+    @IBAction func openPage() {
+        let pageURL = URL(string: "https://bankex.com/en/sto/asset-management")!
+        
+        UIApplication.shared.openURL(pageURL)
+    }
+    
+    @IBAction func openTransaction() {
+        guard let trHash = trHash else { return }
+        
+        let pageURL = URL(string: "https://etherscan.io/tx/\(trHash)")!
+        
+        UIApplication.shared.openURL(pageURL)
+    }
+
+}
+
+extension ContactViewController: MFMailComposeViewControllerDelegate {
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
