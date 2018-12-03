09685b7512a7a20cac80581b9d5667b974485b48
Oleg Kolomyitesv <o.kolomyitsev@bankex.com>
2018-12-03 13:22:40 +0300

    Empty browser was added

---
+++ BankexWallet/AssetManagement/Reusable/AssetManagementBrowser/AssetManagementBrowserViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AssetManagementBrowserViewController.swift
+//  BankexWallet
+//
+//  Created by Oleg Kolomyitsev on 03/12/2018.
+//  Copyright © 2018 Alexander Vlasov. All rights reserved.
+//
+
+import UIKit
+
+class AssetManagementBrowserViewController: UIViewController {
+    
+}
