0e9a124c45eb7f644b269e1caadb062181afd212
Andrew Kozlov <a.kozlov@bankex.com>
2018-12-19 11:13:50 +0300

    [BP-163] TransactionDetailsViewController added

---
+++ BankexWallet/TransactionDetails/TransactionDetailsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TransactionDetailsViewController.swift
+//  BankexWallet
+//
+//  Created by Andrew Kozlov on 19/12/2018.
+//  Copyright © 2018 BANKEX Foundation. All rights reserved.
+//
+
+import UIKit
+
+class TransactionDetailsViewController: UIViewController {
+    
+    var transaction: ETHTransactionModel?
+    
+}
