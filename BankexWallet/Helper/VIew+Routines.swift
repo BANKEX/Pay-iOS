52af9f89dec160262efe88c234b9f584ca52f9db
Andrew Kozlov <a.kozlov@bankex.com>
2019-01-10 11:23:25 +0300

    [BP-250] VIew+Routines unused functions removed

--- BankexWallet/Helper/VIew+Routines.swift
+++ BankexWallet/Helper/VIew+Routines.swift
@@ -9,31 +9,6 @@
 import Foundation
 import UIKit
 
-func CreateVC(byName controllerName: String, probablyStoryboard: UIStoryboard? = nil) -> UIViewController? {
-    var result: UIViewController?
-    
-    if probablyStoryboard != nil {
-        result = TryLoadVC(withName: controllerName, fromStoryboard: probablyStoryboard!)
-    }
-    
-    if result == nil {
-        let loadedStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        result = TryLoadVC(withName: controllerName, fromStoryboard: loadedStoryboard)
-    }
-    
-    return result
-}
-
 var footerTableView:UIView {
     return UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 1))
-}
-
-
-func TryLoadVC(withName controllerName: String, fromStoryboard storyboard: UIStoryboard) -> UIViewController? {
-    var result: UIViewController?
-    
-    do {
-        result = storyboard.instantiateViewController(withIdentifier: controllerName)
-    }
-    return result
 }
