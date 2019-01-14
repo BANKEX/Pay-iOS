e340ee407a8aaa4a100d634ffafa6ac4f00fc118
Vladislav <vladaccess@outlook.com>
2019-01-14 17:41:04 +0300

    [BP 252] Added protocol

---
+++ BankexWallet/InterfaceElements/DesignElements/Main/NavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  NavigationController.swift
+//  BankexWallet
+//
+//  Created by Vladislav on 14/01/2019.
+//  Copyright © 2019 BANKEX Foundation. All rights reserved.
+//
+
+import UIKit
+
+protocol NavigationBarAppearanceProvider {
+    var navigationBarAppearance : NavigationBarAppearance? { get }
+}
